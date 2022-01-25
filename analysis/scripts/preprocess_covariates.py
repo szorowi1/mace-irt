d9bafea826d4bb3cf4a4d3d418e9733f8c2e8ff6
szorowi1 <szorowi1@gmail.com>
2022-01-25 13:20:26 -0500

    repo reorganization

---
+++ analysis/scripts/preprocess_covariates.py
@@ -0,0 +1,135 @@
+import os, json
+import numpy as np
+from os.path import dirname
+from pandas import DataFrame, concat, read_csv
+ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+RAW_DIR = os.path.join(ROOT_DIR, 'raw')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Teicher & Parigger (2015)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Load data.
+teicher2015 = read_csv(os.path.join(ROOT_DIR, 'raw', 'teicher2015', 'pone.0117423.s018.csv'))
+
+## Define columns of interest.
+cols = ['Username','Gender','Age','race','ethnicity','sibs','SQ_Anx','SQ_Dep','SQ_Som','SQ_Hos',
+        'DES_SCORE','LSCL33','ASIQ_tot']
+
+## Restrict to columns of interest.
+teicher2015 = teicher2015[cols].copy()
+
+## Format columns.
+teicher2015 = teicher2015.rename(columns={
+    'Username':'subject',
+    'Gender':'gender',
+    'Age':'age',
+    'sibs':'siblings',
+    'SQ_Anx':'anxiety',
+    'SQ_Dep':'depression',
+    'SQ_Som':'somatic',
+    'SQ_Hos':'hostility',
+    'DES_SCORE':'dissociation',
+    'LSCL33':'irritability',
+    'ASIQ_tot':'suicidality' 
+})
+
+## Format data.
+teicher2015['ethnicity'] = teicher2015.ethnicity.replace({
+    'Not Hisp': 'Not Hispanic or Latino',
+    'Hisp': 'Hispanic or Latino'
+})
+teicher2015['race'] = teicher2015.race.replace({
+    'Black': 'Black or African American'
+})
+teicher2015['gender'] = teicher2015.gender.replace({
+    'M': 'Male',
+    'F': 'Female'
+})
+teicher2015['age'] = teicher2015.age.round(2)
+
+## Insert study.
+teicher2015.insert(0, 'study', 'teicher2015')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Tuominen & Zorowitz (2022)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Locate files.
+fdir = os.path.join(ROOT_DIR, 'raw', 'tuominen2022')
+files = sorted([f for f in os.listdir(fdir) if f.endswith('.json')])
+
+## Preallocate space.
+tuominen2022 = []
+
+for f in files:
+    
+    ## Load file.
+    subject = f.replace('.json','')
+
+    with open(os.path.join(fdir, f), 'r') as f:
+        JSON = json.load(f)
+    
+    ## Initialize dictionary.
+    dd = dict(subject=subject)
+    
+    ## Gather demographics info.
+    demo, = [dd for dd in JSON if dd['trial_type'] == 'survey-demo']
+    dd.update(demo['responses'])
+    
+    ## Format data.
+    dd['siblings'] = int(dd['siblings']) if 'siblings' in dd else np.nan
+    
+    ## Gather infrequency items.
+    infreq = 0
+    infreq += sum([dd.get('infrequency', 0) for dd in JSON if dd.get('survey', '') == 'bisbas'])
+    infreq += sum([dd.get('infrequency', 0) for dd in JSON if dd.get('survey', '') == 'sns'])
+    infreq += sum([dd.get('infrequency', 0) for dd in JSON if dd.get('survey', '') == 'map'])
+    infreq += sum([dd.get('instructed', 0) for dd in JSON if dd.get('survey', '') == 'mace'])
+    dd['infreq'] = infreq
+    
+    ## Gather honeypot.
+    bot = 0
+    bot += sum([dd.get('honeypot', 0) for dd in JSON if dd.get('survey', '') == 'bisbas'])
+    bot += sum([dd.get('honeypot', 0) for dd in JSON if dd.get('survey', '') == 'sns'])
+    bot += sum([dd.get('honeypot', 0) for dd in JSON if dd.get('survey', '') == 'map'])
+    bot += sum([dd.get('honeypot', 0) for dd in JSON if dd.get('survey', '') == 'mace'])
+    dd['bot'] = bot
+    
+    ## Append info.
+    tuominen2022.append(dd)
+    
+## Concatenate data.
+tuominen2022 = DataFrame(tuominen2022)
+
+## Restrict to columns of interest.
+drop_cols = ['gender-free-response', 'education']
+tuominen2022 = tuominen2022.drop(columns=drop_cols).rename(columns={'gender-categorical':'gender'})
+
+## Format data.
+tuominen2022['race'] = tuominen2022.race.apply(lambda x: 'Multiracial' if len(x) > 1 else x[0])
+tuominen2022['age'] = tuominen2022.age.astype(float).round(2)
+
+## Insert study.
+tuominen2022.insert(0, 'study', 'tuominen2022')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Merge & save DataFrames.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Concatenate DataFrames.
+data = concat([
+    teicher2015,
+    tuominen2022
+])
+
+## Re-index subjects.
+f = lambda x: x.study + '_' + x.subject
+data['subject'] = np.unique(data.apply(f, 1), return_inverse=True)[-1] + 1
+
+## Format data.
+data[['infreq','bot']] = data[['infreq','bot']].fillna(0)
+
+## Save data.
+data.to_csv(os.path.join(DATA_DIR, 'covariates.csv'), index=False)
---
+++ analysis/scripts/preprocess_mace.py
@@ -0,0 +1,127 @@
+import os, json
+import numpy as np
+from os.path import dirname
+from pandas import DataFrame, concat, read_csv
+ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+RAW_DIR = os.path.join(ROOT_DIR, 'raw')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Define parameters.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Load mapping file.
+mapping = read_csv(os.path.join(ROOT_DIR, 'raw', 'mapping.csv'))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Teicher & Parigger (2015)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Load data.
+teicher2015 = read_csv(os.path.join(ROOT_DIR, 'raw', 'teicher2015', 'pone.0117423.s018.csv'))
+
+## Set index.
+teicher2015 = teicher2015.rename(columns={'Username':'subject'}).set_index('subject')
+teicher2015.index = [s.lower() for s in teicher2015.index]
+
+## Limit to MACE items & melt.
+teicher2015 = teicher2015[mapping.item_id].copy()
+teicher2015 = teicher2015.melt(var_name='item_id', value_name='response', ignore_index=False)
+teicher2015 = teicher2015.reset_index().rename(columns={'index':'subject'})
+
+## Merge with mapping.
+teicher2015 = teicher2015.merge(mapping, on='item_id')
+
+## Insert study.
+teicher2015.insert(0, 'study', 'teicher2015')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Tuominen & Zorowitz (2022)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Locate files.
+fdir = os.path.join(ROOT_DIR, 'raw', 'tuominen2022')
+files = sorted([f for f in os.listdir(fdir) if f.endswith('.json')])
+
+## Preallocate space.
+tuominen2022 = []
+for f in files:
+    
+    ## Load file.
+    subject = f.replace('.json','')
+
+    with open(os.path.join(fdir, f), 'r') as f:
+        JSON = json.load(f)
+    
+    ## Gather siblings data.
+    demo, = [dd for dd in JSON if dd['trial_type'] == 'survey-demo']
+    siblings = int(demo['responses'].get('siblings', 0))
+    
+    ## Gather MACE data.
+    mace = [dd for dd in JSON if 'survey' in dd and dd['survey']=='mace']
+    
+    ## Preallocate space.
+    responses = np.zeros((53, 2))
+    
+    ## Iterate over responses.
+    current_item = -1
+    for k, v in [(k,v) for dd in mace for k,v in dd['responses'].items()]:
+        
+        ## Update current item.
+        if 'ages' not in k: current_item += 1
+            
+        ## Indicate response.
+        if 'ages' not in k:
+            responses[current_item,0] = 1 if v == 'Yes' else 0
+        
+        ## Increment age counts.
+        else:
+            responses[current_item,1] += 1
+            
+    ## Convert to DataFrame.
+    df = DataFrame(responses, columns=['response','n_years']).drop(50)
+    
+    ## Insert metadata. 
+    df.insert(0, 'subject', subject)
+    df.insert(1, 'item', np.arange(len(df))+1)
+    
+    ## Score WSV subscale.
+    df.loc[np.in1d(df.item, [15, 16, 17, 18]), ['response','n_years']] *= 1 if siblings else np.nan
+    
+    ## Append.
+    tuominen2022.append(df)
+    
+## Concatenate data.
+tuominen2022 = concat(tuominen2022)
+
+## Merge with mapping.
+tuominen2022 = tuominen2022.merge(mapping, on='item')
+
+## Insert study.
+tuominen2022.insert(0, 'study', 'tuominen2022')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Merge & save DataFrames.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Concatenate DataFrames.
+data = concat([
+    teicher2015,
+    tuominen2022
+])
+
+## Re-index subjects.
+f = lambda x: x.study + '_' + x.subject
+data['subject'] = np.unique(data.apply(f, 1), return_inverse=True)[-1] + 1
+
+## Re-organize columns.
+cols = ['study','subject','item','item_id','subscale','response','n_years']
+data = data[cols].sort_values(['subject','item'])
+
+## Reverse score items.
+indices = np.in1d(data.item, [42, 43, 44, 45, 51, 52])
+data.loc[indices, 'response'] = 1 - data.loc[indices, 'response']
+data.loc[indices, 'n_years'] = 18 - data.loc[indices, 'n_years']
+
+## Save data.
+data.to_csv(os.path.join(DATA_DIR, 'mace.csv'), index=False)
